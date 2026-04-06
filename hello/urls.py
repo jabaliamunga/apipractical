c594e229181e707d818ac6d47bad522b60682c3a
Jabaliamunga <jabalamunga@gmail.com>
2026-04-06 18:21:44 +0300

    Add delete view and update HTML for student records

--- hello/urls.py
+++ hello/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     path("get_students/", views.get_students),       #api endpoint to get students
     path("store_students/", views.store_students),    #api endpoint to store students
     path("", views.front, name='front'),  #randers template directly to the front end
-    path("fetchdata/", views.fetchdata, name='fetchdata')
+    path("fetchdata/", views.fetchdata, name='fetchdata'),
+    path('delete_student/<int:pk>/', views.delete_student, name='delete_student'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework.decorators import api_view #for funtion based views
 from rest_framework.response import Response
 from .models import Student
@@ -22,6 +22,16 @@ def store_students(request):
         serializer.save()  #save serializer to database
         return redirect('fetchdata')
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) #api endpoint to store data
+
+@api_view(['DELETE', 'POST'])
+def delete_student(request, pk):
+    if request.method =='POST':
+        student = get_object_or_404(Student, pk=pk)
+        student.delete()
+        return redirect('fetchdata')
+    return redirect('fetchdata')
+
+
     
 
 def fetchdata(request):
